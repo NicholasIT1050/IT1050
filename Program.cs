8f3a91afee4bf97b691e95db7f8031fb24673732
NicholasIT1050 <56942249+NicholasIT1050@users.noreply.github.com>
2019-10-23 19:48:41 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Week_1_Nicholas_M
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str1, str2;str1 = "Hello World"; str2 = "Nicholas";
+            Console.WriteLine(str1 + " from " + str2);
+        }
+    }
+}
