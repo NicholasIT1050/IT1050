19c71b6f3b0216aeb586dd2bd86e6ad2c34ce73a
NicholasIT1050 <56942249+NicholasIT1050@users.noreply.github.com>
2019-11-18 22:11:54 -0500

    Add files via upload

---
+++ Lab4/Program9ab.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab_4._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int j;
+            
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine("**********".Substring(0, i));
+            }
+            for (j = 10; j > 0; j--)
+            {
+                Console.WriteLine("**********".Substring(0, j));
+            }
+            
+            
+        }
+    }
+}
---
+++ Lab4/Program9cd.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lab_4._10
+{
+    class Program
+    {
+        static void Main()
+        {
+            int counter = 10;
+
+            for (int r = 0; r < counter; r++)
+            {
+                for (int c = 0; c < r; c++) 
+                {
+                    Console.Write(" ");
+                }
+                for (int c = 0; c < counter - r; c++) 
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            for (int r = counter; r >= 0; r--) 
+            {
+                for (int c = 0; c < r; c++)
+                {
+                    Console.Write(" ");
+                }
+                for (int c = 0; c < counter - r; c++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
