3a5f1ffb8589909ffb39de8f0293057eb490afd1
NicholasIT1050 <56942249+NicholasIT1050@users.noreply.github.com>
2019-12-02 15:28:35 -0500

    Add files via upload

---
+++ Lab6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab6._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namesofpeople = { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
+            int i = 0;
+            foreach (string names in namesofpeople)
+            {
+                Console.WriteLine("{0} {1}", i + 1, names);
+                i++;
+            }
+        }
+    }
+}
