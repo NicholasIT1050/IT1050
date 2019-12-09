cfd583f75a8c0fe09e4291be9843f437d9d9e3d7
NicholasIT1050 <56942249+NicholasIT1050@users.noreply.github.com>
2019-12-08 22:09:28 -0500

    Add files via upload

---
+++ Lab7/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab7
+{
+    class Book
+    {
+        static void Main(string[] args)
+        {
+            BookTest myBook = new BookTest("Percy Jackson and the Lightning Theif", "Rick Riordan", 2006);
+
+            myBook.Display();
+        }
+
+    }
+}
+
---
+++ Lab7/BookTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab7
+{
+    class BookTest
+    {
+
+        private string Title;
+        private string Author;
+        private int Year;
+        public BookTest(string Title, string Author, int Year)
+        {
+            this.Title = Title;
+            this.Author = Author;
+            this.Year = Year;
+        }
+        public void Display()
+        {
+            Console.WriteLine("{0} was written by {1} released in {2}", Title, Author, Year);
+        }
+    }
+} 
